5e4cd38deef76397d97dc9bbc485e90ecbf88151
Renato Paiva <paivarenato08@gmail.com>
2022-09-18 18:35:05 -0300

    finalizando a tarefa quando o temporizador acabar

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,31 +1,48 @@
-import  Botao from '../Botao';
-import Relogio from './Relogio';    
+import Botao from "../Botao";
+import Relogio from "./Relogio";
 import style from './Cronometro.module.scss';
-import { tempoParaSegundos } from '../../common/utils/time';
-import { ITarefa } from '../../types/tarefa';
-import { useEffect, useState } from 'react';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
 
 interface Props {
-    selecionado: ITarefa | undefined
+selecionado: ITarefa | undefined,
+finalizarTarefa: () => void
 }
 
-export default function Cronometro({ selecionado }: Props) {
-    const [tempo, setTempo] = useState<number>();
+export default function Cronometro({ selecionado, finalizarTarefa }: Props) {
+const [tempo, setTempo] = useState<number>();
 
 useEffect(() => {
     if(selecionado?.tempo) {
-        setTempo(tempoParaSegundos(selecionado.tempo));
+    setTempo(tempoParaSegundos(selecionado.tempo));
     }
-},[selecionado])
+},[selecionado]);
+
+function regressiva(contador: number = 0) {
+    setTimeout(() => {
+    if(contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+    }
+    finalizarTarefa();
+    }, 1000);
+}
+
+
 return (
         <div className={style.cronometro}>
             <p className={style.título}>Escolha um card e inicie o cronometro</p>
             <div className={style.relogioWrapper}>
             <Relogio tempo={tempo} />
             </div>
-            <Botao>
+            <Botao onClick={() => regressiva(tempo)}>
                 Começar!
             </Botao>
         </div>
     )
+}
+
+function finalizarTarefa() {
+    throw new Error('Function not implemented.');
 }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -17,14 +17,33 @@ function App() {
   })));
   }
 
-  return (
+    function finalizarTarefa() {
+        if(selecionado) {
+            setTarefas(tarefasAnteriores =>
+            tarefasAnteriores.map(tarefa => {
+                if(tarefa.id === selecionado.id) {
+                    return {
+                        ...tarefa,
+                        selecionado: false,
+                        completado: true
+                    }
+                }
+                return tarefa;
+            }))
+        }
+    }
+
+return (
     <div className={style.AppStyle}>
-      <Formulario  setTarefas={setTarefas} />
-      <Lista 
-      tarefas={tarefas}
-      selecionaTarefa={selecionaTarefa}
+      <Formulario setTarefas={setTarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
       />
-      <Cronometro selecionado={selecionado}/>
+      <Cronometro
+        selecionado={selecionado}
+        finalizarTarefa={finalizarTarefa}
+      />
     </div>
   );
 }
